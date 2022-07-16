e8c485501b30b2066b62881d52a4ee645f3ea4d4
Pavel Popkov <goil2011@yandex.ru>
2022-07-16 16:46:26 +0300

    number3

---
+++ number3/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 || n == 7)
+    Console.WriteLine("Выходной");
+if (n > 7)
+    Console.WriteLine("Это не день недели");
+if (n < 6)
+    Console.WriteLine("Рабочий день!");
