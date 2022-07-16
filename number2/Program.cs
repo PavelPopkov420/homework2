0b0ed0d4185b8548071380d9bfaedbd3a8a6e0ba
Pavel Popkov <goil2011@yandex.ru>
2022-07-16 16:37:46 +0300

    number2

---
+++ number2/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 100 && n < 999)
+{
+    n = n % 10;
+    Console.WriteLine("Третье чисто: " + Convert.ToString(n));
+}
+else 
+    Console.WriteLine("Третьего числа нет");
