c29d903208c76934a3e8b05cbfa076a1593f32c8
Pavel Popkov <goil2011@yandex.ru>
2022-07-16 16:18:36 +0300

    number1

---
+++ number1/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 100 || n > 999)
+    {
+    n = (n / 10) % 10;
+    Console.WriteLine("Второе чисто: " + Convert.ToString(n));
+    }
+else
+    Console.WriteLine("Введите трехзначное число: ");
