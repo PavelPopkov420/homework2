154f879e110b56ca1db5a9ada1389769366e0207
Pavel Popkov <goil2011@yandex.ru>
2022-07-16 16:52:13 +0300

    Корректное решение

--- number1/Program.cs
+++ number1/Program.cs
@@ -1,6 +1,6 @@
 ﻿int n = Convert.ToInt32(Console.ReadLine());
 
-if (n > 100 || n > 999)
+if (n > 100 && n < 999)
     {
     n = (n / 10) % 10;
     Console.WriteLine("Второе чисто: " + Convert.ToString(n));
